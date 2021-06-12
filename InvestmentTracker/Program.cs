b6b6a7b846ec8b540843dd4d65502b3e3302f5c1
John Velasco <velascoj@ksu.edu>
2021-06-12 17:58:17 -0500

    Add project files.

---
+++ InvestmentTracker/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestmentTracker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl();
+
+        }
+    }
+}
