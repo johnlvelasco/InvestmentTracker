40553f5ba66a107819a9ef3aa40447ee32d1e31a
johnlvelasco <velascoj@ksu.edu>
2021-06-12 22:09:38 -0500

    Built Main for collecting Ether data.

--- InvestmentTracker/Program.cs
+++ InvestmentTracker/Program.cs
@@ -3,7 +3,8 @@ using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace InvestmentTracker
 {
@@ -11,11 +12,58 @@ namespace InvestmentTracker
     {
         public static void Main(string[] args)
         {
+            Program p = new Program();
 
-
-            IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl();
-
+            //For CoinDesk data collection: 
+            p.CoinDesk(new ChromeDriver()); 
         }
+
+        public void CoinDesk(IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl("http://www.coindesk.com/price/ethereum");
+
+            string price = driver.FindElement(By.ClassName("price-large")).Text; 
+            string dailyChange = driver.FindElement(By.ClassName("percent-value-text")).Text; 
+            string stockChart = driver.FindElement(By.ClassName("stock-chart")).Text; 
+
+            Console.WriteLine("Price: " + price);
+            Console.WriteLine("Daily Change: " + dailyChange);
+
+            //My investments..
+            //original price, date, and NET change since date.
+
+            Thread.Sleep(5000); 
+            //Quit(driver);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void Quit(IWebDriver driver)
+        {
+            driver.Close();
+            driver.Quit();
+            Environment.Exit(0);
+        }
+
+
     }
+
 }
